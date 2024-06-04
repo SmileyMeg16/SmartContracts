44cf08f200453e0ff36a143411178b2d279f3cc1
SmileyMeg16 <megan.e.steeves@gmail.com>
2024-06-04 09:35:37 -0600

    Update BlockchainMessenger.sol

--- BlockchainMessenger.sol
+++ BlockchainMessenger.sol
@@ -1,1 +1,30 @@
+//SPDX-License-Identifier: MIT
 
+// Contract that allows any user to see the message on the chain
+// but only allows the owner to write to the chain. Counts how many
+// times the message was updated.
+
+// Megan Steeves
+// Last Updated 06/04/2024
+
+pragma solidity 0.8.15;
+
+contract BlockchainMessenger {
+
+    uint public changeCounter;
+
+    address public owner;
+
+    string public message;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function UpdateMessage(string memory _newMessage) public {
+        if(msg.sender == owner) {
+            message = _newMessage;
+            changeCounter++;
+        }
+    }
+}
